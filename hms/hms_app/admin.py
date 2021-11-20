15c82fc9141c64686557f5945ae64c1470371ed5
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2021-11-21 01:42:06 +0530

    create database and admin page name to admins

--- hms/hms_app/admin.py
+++ hms/hms_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from hms_app.models import Admin,Housekeeper,Room_floor,Room,Room_details
 # Register your models here.
+admin.site.register(Admin),
+admin.site.register(Housekeeper),
+admin.site.register(Room_floor),
+admin.site.register(Room),
+admin.site.register(Room_details),
--- hms/hms_app/views.py
+++ hms/hms_app/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(requests):
-    return render(requests , 'admin/index.html')
+    return render(requests , 'admins/index.html')
 
 def room_status(requests):
     if requests.method == 'POST':
@@ -13,11 +13,11 @@ def room_status(requests):
         my_dict = { 
                "clicked" : name,
                }
-        return render(requests, 'admin/room_status.html', context=my_dict)
+        return render(requests, 'admins/room_status.html', context=my_dict)
     else:
        
-        return render(requests, 'admin/room_status.html')
+        return render(requests, 'admins/room_status.html')
 
 
 def room_manage(request):
-    return render(request, 'admin/room_manage.html')
+    return render(request, 'admins/room_manage.html')
