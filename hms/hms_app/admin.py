2466f5a7f89345db7071200ae63da9197bb780d3
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2021-11-21 02:04:18 +0530

    create more required model like housekpper_details adn fixed some bug

--- hms/hms_app/admin.py
+++ hms/hms_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from hms_app.models import Admin,Housekeeper,Room_floor,Room,Room_details
+from hms_app.models import Admin,Housekeeper,Room_floor,Room,Room_details,Housekeeper_details,Housekeeper_room_visit
 # Register your models here.
 admin.site.register(Admin),
 admin.site.register(Housekeeper),
 admin.site.register(Room_floor),
 admin.site.register(Room),
 admin.site.register(Room_details),
+admin.site.register(Housekeeper_details),
+admin.site.register(Housekeeper_room_visit),
--- hms/hms_app/models.py
+++ hms/hms_app/models.py
@@ -14,7 +14,7 @@ class Housekeeper(models.Model):
     housekeeper_name = models.CharField(max_length=30)
     housekeeper_email = models.CharField(max_length=40)
     housekeeper_password = models.CharField(max_length=30)
-    
+
     
 class Room_floor(models.Model):
     floor_id = models.AutoField(primary_key=True)
@@ -35,10 +35,20 @@ class Room_details(models.Model):
     room_occupancy = models.CharField(max_length=15)
     room_status = models.CharField(max_length=15)
     room_notes = models.CharField(max_length=250)
+    room_inspect_status = models.CharField(max_length=20, null=True)
     room_housekeeper = models.ForeignKey(Housekeeper, on_delete=models.CASCADE)
     room_updated_by = models.ForeignKey(Admin, on_delete=models.CASCADE)
     room_updated_time = models.DateTimeField(auto_now_add=True, null=True)
     
+class Housekeeper_details(models.Model):
+    id = models.AutoField(primary_key=True)
+    housekeeper_id = models.ForeignKey(Housekeeper, on_delete=models.CASCADE)
+    housekeeper_status = models.CharField(max_length=20)
+
+class Housekeeper_room_visit(models.Model):
+    id = models.AutoField(primary_key=True)
+    housekeeper_id = models.ForeignKey(Housekeeper, on_delete=models.CASCADE)
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
     
     
     
