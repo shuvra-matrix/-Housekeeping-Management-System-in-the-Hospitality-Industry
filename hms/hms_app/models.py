aa9da96c51a6d0a43dd7c9d2ca4736b1fb95d7a4
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2021-11-27 04:54:24 +0530

    stable update 3.5.1

--- hms/hms_app/models.py
+++ hms/hms_app/models.py
@@ -40,12 +40,12 @@ class Room_floor(models.Model):
     
 class Room(models.Model):
     room_id = models.AutoField(primary_key=True)
-    floor_id = models.ForeignKey(Room_floor, on_delete=models.CASCADE, related_name='floors_id')
+    floor_id = models.ForeignKey(Room_floor, on_delete=models.SET_NULL,null=True)
     room_name = models.CharField(max_length=30)
     
 class Room_details(models.Model):
     id = models.AutoField(primary_key=True)
-    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+    room_id = models.ForeignKey(Room, on_delete=models.SET_NULL,null=True)
     room_type = models.CharField(max_length=20)
     room_status = models.CharField(max_length=20,default="Active")
     room_reservation_info = models.CharField(max_length=20,null=True)
@@ -53,7 +53,7 @@ class Room_details(models.Model):
     room_notes = models.CharField(max_length=250,null=True)
     room_housekeeper_note = models.CharField(max_length=255,null=True)
     room_inspect_status = models.CharField(max_length=20, default="Inspected")
-    room_housekeeper = models.ForeignKey(Housekeeper, on_delete=models.DO_NOTHING,null=True)
+    room_housekeeper = models.ForeignKey(Housekeeper, on_delete=models.SET_NULL,null=True)
     room_updated_by = models.CharField(max_length=50 ,null=True)
     room_updated_time = models.CharField(max_length=20, null=True)
     
@@ -61,7 +61,7 @@ class Room_details(models.Model):
 class Housekeeper_room_visit(models.Model):
     id = models.AutoField(primary_key=True)
     housekeeper_id = models.ForeignKey(Housekeeper, on_delete=models.CASCADE)
-    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+    room_id = models.ForeignKey(Room, on_delete=models.SET_NULL,null=True)
 
     
 class Housekeeper_details(models.Model):
@@ -75,26 +75,30 @@ class Food_type(models.Model):
 
 class Food_drinks(models.Model):
     id=models.AutoField(primary_key=True)
-    food_type = models.ForeignKey(Food_type,on_delete=models.CASCADE)
+    food_type = models.ForeignKey(Food_type,on_delete=models.SET_NULL,null=True)
     food_name = models.CharField(max_length=20)
 
 class Food_quentity(models.Model):
     id = models.AutoField(primary_key=True)
-    food_type = models.ForeignKey(Food_type,on_delete=CASCADE)
+    food_type = models.ForeignKey(
+        Food_type, on_delete=models.SET_NULL, null=True)
     quentity = models.CharField(max_length=20)
 
 
 class Food_order_list(models.Model):
     id = models.AutoField(primary_key=True)
-    room = models.ForeignKey(Room, on_delete=models.DO_NOTHING)
-    food_name = models.ForeignKey(Food_drinks, on_delete=models.DO_NOTHING)
-    quentity = models.ForeignKey(Food_quentity, on_delete=models.DO_NOTHING)
+    room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
+    food_name = models.ForeignKey(
+        Food_drinks, on_delete=models.SET_NULL, null=True)
+    quentity = models.ForeignKey(
+        Food_quentity, on_delete=models.SET_NULL, null=True)
     show_list = models.CharField(max_length=5, default="yes")
 
 class Room_service(models.Model):
     id = models.AutoField(primary_key=True)
-    room_id = models.ForeignKey(Room,on_delete=models.CASCADE)
-    food_list_id = models.ForeignKey(Food_order_list, on_delete=models.DO_NOTHING)
+    room_id = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
+    food_list_id = models.ForeignKey(
+        Food_order_list, on_delete=models.SET_NULL, null=True)
     order_taken_by = models.CharField(max_length=50, null=True)
     order_status = models.CharField(max_length=20,default="Under-Cooking")
     time = models.CharField(max_length=20, null=True)
@@ -103,7 +107,7 @@ class Room_service(models.Model):
 class Customer_complaints(models.Model):
     id = models.AutoField(primary_key=True)
     complaints = models.CharField(max_length=255)
-    room_id = models.ForeignKey(Room, on_delete=models.DO_NOTHING)
+    room_id = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
     complaints_taken_by = models.CharField(max_length=50)
     complaints_by = models.CharField(max_length=50)
     time = models.CharField(max_length=20)
@@ -116,7 +120,8 @@ class Daily_activities(models.Model):
         
 class Housekeeping_daily_activity(models.Model):
     id = models.AutoField(primary_key=True)
-    activity = models.ForeignKey(Daily_activities, on_delete=models.CASCADE)
+    activity = models.ForeignKey(
+        Daily_activities, on_delete=models.SET_NULL, null=True)
     time = models.CharField(max_length=10, default="00000000")
 
 
